0499ffc29d6904efdc3b625496aa600ed53dbe26
KuroKenshi16 <igor10gondo@gmail.com>
2022-05-30 14:28:18 -0300

    Inserção de arquivos do tipo sql

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,7 +1,15 @@
 <?php
 
     // Carregar as credencias do banco de dados
-    $hostname = "";
-    $database = "";
-    $user = "";
-    $password = "";
+    $hostname = "sql300.epizy.com";
+    $database = "epiz_31642488_sistema_de_controle_de_rifas";
+    $user = "epiz_31642488";
+    $password = "ULuWksPalsBIWmI";
+
+    try{
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $password);
+        $pdo->setAttriute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexão com o banco de dados '.$database.', foi realizado com sucesso!';
+    }catch (PDOException $e) {
+       echo 'Erro: '.$e->getMessage();
+    }
